3588e9b011918c75ab709c307a42869c766ae16a
tkawaguchi0722 <tkawaguchi@bltinc.co.jp>
2025-08-15 16:30:23 +0900

    fix:環境変数を読み取れるようにviteを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  const apiTarget = process.env.VITE_API_TARGET || 'https://localhost:8090'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiTarget = process.env.VITE_API_TARGET || env.VITE_API_TARGET 
   
   return {
+  define: {
+    'process.env.VITE_API_TARGET': JSON.stringify(apiTarget),
+  },
   plugins: [
     TanStackRouterVite({
       target: 'react',
